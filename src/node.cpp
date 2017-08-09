9e4ee5a30dfc79cb6dfc5a49acbccaf81c65ec79
gavanderhoorn <g.a.vanderhoorn@tudelft.nl>
2017-08-09 14:10:05 +0200

    Initial import.

---
+++ src/node.cpp
@@ -0,0 +1,28 @@
+
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+
+
+int main(int argc, char *argv[])
+{
+	ros::init(argc, argv, "test_node");
+	ros::NodeHandle nh;
+	ros::Publisher pub = nh.advertise<std_msgs::String>("test_topic", 1);
+	ros::Rate loop_rate(1);
+	int c = 0;
+
+	while (ros::ok())
+	{
+		std_msgs::String msg;
+
+		std::stringstream ss;
+		ss << "bliep " << c++;
+		msg.data = ss.str();
+
+		pub.publish(msg);
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+
+	return 0;
+}
