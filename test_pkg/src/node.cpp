5cfb64a2e50b8e4a47ee73839d465665d8989068
Jonathan Hechtbauer <jonathan.hechtbauer@ipa.fraunhofer.de>
2018-02-12 15:32:34 +0100

    Converted tabs to whitespaces

--- test_pkg/src/node.cpp
+++ test_pkg/src/node.cpp
@@ -5,24 +5,24 @@
 
 int main(int argc, char *argv[])
 {
-	ros::init(argc, argv, "test_node");
-	ros::NodeHandle nh;
-	ros::Publisher pub = nh.advertise<std_msgs::String>("test_topic", 1);
-	ros::Rate loop_rate(1);
-	int c = 0;
+  ros::init(argc, argv, "test_node");
+  ros::NodeHandle nh;
+  ros::Publisher pub = nh.advertise<std_msgs::String>("test_topic", 1);
+  ros::Rate loop_rate(1);
+  int c = 0;
 
-	while (ros::ok())
-	{
-		std_msgs::String msg;
+  while (ros::ok())
+  {
+    std_msgs::String msg;
 
-		std::stringstream ss;
-		ss << "bliep " << c++;
-		msg.data = ss.str();
+    std::stringstream ss;
+    ss << "bliep " << c++;
+    msg.data = ss.str();
 
-		pub.publish(msg);
-		ros::spinOnce();
-		loop_rate.sleep();
-	}
+    pub.publish(msg);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
 
 	return 0;
 }
